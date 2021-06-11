b554e7fb4ea2842d03807abbe51ae1287ebced41
Muhammad Daffa <36522826+daffainfo@users.noreply.github.com>
2021-06-11 23:32:20 +0700

    Update client.c

--- client/client.c
+++ client/client.c
@@ -1,28 +1,134 @@
 //Masih logika sama error handling gaes
-
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 //BUAT CHECK ROOT ATAU BUKAN
-int checkSudo() {
-    uid_t uid=getuid(), euid=geteuid();
-    if(uid != 0) {
-     return 0; // Login as user
-    } else {
-     return 1; //Login as root
-    }
-}
-
-//Check login
-if (strcmp(argv[1], "-u") == 0 && argv[2] != 0) {
-       if (strcmp(argv[3], "-p") == 0 && argv[4] != 0) {
-             if (user & pass ada) {
-                    //login
-             } else {
-                    // user / password salah
-             }
-       } else {
-              //echo "Butuh password"
-       }
-} else {
-       //echo "Welcome, to use this program add argument -u for user and -p for password"
+int checkSudo()
+{
+	uid_t uid = getuid(), euid = geteuid();
+	if (uid != 0)
+	{
+		return 0; // Login as user
+	}
+	else
+	{
+		return 1; //Login as root
+	}
 }
 
 //Setelah login
+
+//FUNGSI BUAT NGAMBIL SPECifi WORD DI STRING
+void nthword(char *str, int num)
+{
+	char ret[80];
+
+	int i = 0, strnum = 0;
+    char *prev;
+	prev = NULL;
+
+	while (*str != '\0')
+	{
+		if (*str != ' ')
+		{
+			if ((prev == NULL) || (*prev == ' '))
+				strnum++;
+			if (strnum == num)
+			{
+				ret[i] = *str;
+				i++;
+			}
+			str++;
+			prev = str - 1;
+		}
+		else if (*str == ' ')
+		{
+			str++;
+			prev = str - 1;
+		}
+	}
+
+	ret[i] = '\0';
+	printf("%s \n", ret);
+}
+
+int main() {
+	//Check login
+	if (strcmp(argv[1], "-u") == 0 && argv[2] != 0)
+	{
+		if (strcmp(argv[3], "-p") == 0 && argv[4] != 0)
+		{
+			if (user & pass ada)
+			{
+				//login
+			}
+			else
+			{
+				// user / password salah
+			}
+		}
+		else
+		{
+			//echo "Butuh password"
+		}
+	}
+	else
+	{
+		//echo "Welcome, to use this program add argument -u for user and -p for password"
+	}
+
+    //Buat create user ini
+	// char *test = "CREATE USER jack IDENTIFIED BY jack123;";
+    if (strcmp(nthword(test, 1), "CREATE") == 0 && strcmp(nthword(test, 2), "USER") == 0 )
+    {
+        if (strcmp(nthword(test, 4), "IDENTIFIED") == 0 && strcmp(nthword(test, 5), "BY") == 0 )
+        {
+            strtok(nthwork(test, 6), ";");
+            // echo create user sucess
+        }
+        else
+        {
+            // password yang bener
+        }
+    }
+    else
+    {
+        //user yang bener
+    }
+    
+    //Buat use db
+	// char *test = "USE DATABASE1;";
+    if (strcmp(nthword(test, 1), "USE") == 0 && nthword(test, 2) != 0)
+    {
+        char *dapos = strtok(nthwork(test, 2), ";");
+        getcwd() //semisal di /home/daffainfo/fp/
+        
+        if (chdir("/databases/${dapos}") == 0) //jika benar
+        {
+            //database dapos used   
+        }
+        else
+        {
+            //database not found
+        }
+    }
+    
+    //Buat create db
+	// char *test = "CREATE DATABASE dapos;";
+    if (strcmp(nthword(test, 1), "CREATE") == 0 && strcmp(nthword(test, 1), "DATABASE") == 0)
+    {
+        char *dapos = strtok(nthwork(test, 3), ";");
+        chdir("/databases");
+        mkdir(dapos);
+        
+        if (mkdir(dapos) == 0) //jika berhasil
+        {
+            //database dapos created  
+        }
+        else
+        {
+            //database is exist
+        }
+    }
+	return 0;
+}
