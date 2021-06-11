d2bc9300618b6b3442cdfd5746025c5f7efac039
Muhammad Daffa <36522826+daffainfo@users.noreply.github.com>
2021-06-12 01:32:18 +0700

    Update client.c

--- client/client.c
+++ client/client.c
@@ -1,188 +1,49 @@
-//Masih logika sama error handling gaes
 #include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <netinet/in.h>
 #include <string.h>
-#include <ctype.h>
-//BUAT CHECK ROOT ATAU BUKAN
-int checkSudo()
-{
-	uid_t uid = getuid(), euid = geteuid();
-	if (uid != 0)
-	{
-		return 0; // Login as user
+#include <unistd.h>
+#include <arpa/inet.h>
+#define PORT 8080
+  
+int main(int argc, char const *argv[]) {
+    struct sockaddr_in address;
+    int sock = 0, valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+  
+    memset(&serv_addr, '0', sizeof(serv_addr));
+  
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+      
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+  
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    while(1) {
+        char buffer[1024] = {0};
+        char *str;
+	printf("Welcome to A09 Database, Please Input Query");
+        scanf("%s",str);
+	if (strcmp(str, "exit") == 0) {
+	    exit(0);
 	}
-	else
-	{
-		return 1; //Login as root
-	}
+        send(sock, str, strlen(str) , 0 );
+        printf("Query Executed\n");
+        valread = read(sock, buffer, 1024);
+        printf("%s\n",buffer);
+
+    }
+    return 0;
 }
-
-//Setelah login
-
-//FUNGSI BUAT NGAMBIL SPECifi WORD DI STRING
-void nthword(char *str, int num)
-{
-	char ret[80];
-
-	int i = 0, strnum = 0;
-	char *prev;
-	prev = NULL;
-
-	while (*str != '\0')
-	{
-		if (*str != ' ')
-		{
-			if ((prev == NULL) || (*prev == ' '))
-				strnum++;
-			if (strnum == num)
-			{
-				ret[i] = *str;
-				i++;
-			}
-			str++;
-			prev = str - 1;
-		}
-		else if (*str == ' ')
-		{
-			str++;
-			prev = str - 1;
-		}
-	}
-
-	ret[i] = '\0';
-	printf("%s \n", ret);
-}
-
-int main()
-{
-	//Check login
-	if (strcmp(argv[1], "-u") == 0 && argv[2] != 0)
-	{
-		if (strcmp(argv[3], "-p") == 0 && argv[4] != 0)
-		{
-			if (user & pass ada)
-			{
-				//login
-			}
-			else
-			{
-				// user / password salah
-			}
-		}
-		else
-		{
-			//echo "Butuh password"
-		}
-	}
-	else
-	{
-		//echo "Welcome, to use this program add argument -u for user and -p for password"
-	}
-
-	//Buat create user ini
-	// char *test = "CREATE USER jack IDENTIFIED BY jack123;";
-	if (strcmp(nthword(test, 1), "CREATE") == 0 && strcmp(nthword(test, 2), "USER") == 0)
-	{
-		if (strcmp(nthword(test, 4), "IDENTIFIED") == 0 && strcmp(nthword(test, 5), "BY") == 0)
-		{
-			strtok(nthwork(test, 6), ";");
-			// echo create user sucess
-		}
-		else
-		{
-			// password yang bener
-		}
-	}
-	else
-	{
-		//user yang bener
-	}
-
-	//Buat use db
-	// char *test = "USE DATABASE1;";
-	if (strcmp(nthword(test, 1), "USE") == 0 && nthword(test, 2) != 0)
-	{
-		char *dapos = strtok(nthwork(test, 2), ";");
-		getcwd() //semisal di /home/daffainfo/fp/
-
-			if (chdir("/databases/${dapos}") == 0) //jika benar
-		{
-			//database dapos used
-		}
-		else
-		{
-			//database not found
-		}
-	}
-
-	//Buat create db
-	// char *test = "CREATE DATABASE dapos;";
-	if (strcmp(nthword(test, 1), "CREATE") == 0 && strcmp(nthword(test, 2), "DATABASE") == 0)
-	{
-		char *dapos = strtok(nthwork(test, 3), ";");
-		chdir("/databases");
-		mkdir(dapos);
-
-		if (mkdir(dapos) == 0) //jika berhasil
-		{
-			//database dapos created
-		}
-		else
-		{
-			//database is exist
-		}
-	}
-	return 0;
-
-	//BUAT DROP ANEH"
-	if (strcmp(nthword(test, 1), "DROP") == 0)
-	{
-		if (strcmp(nthword(test, 2), "DATABASE") == 0)
-		{
-			char *dapos = strtok(nthwork(test, 3), ";");
-			chdir("/databases");
-			rmdir(dapos);
-			if (rmdir(dapos) == 0) //jika berhasil
-			{
-				//database dapos deleted
-			}
-			else
-			{
-				//database dpos not exist
-			}
-		}
-		else if (strcmp(nthword(test, 2), "TABLES") == 0)
-		{
-			char *dapos = strtok(nthwork(test, 3), ";");
-			//PAKE FUNGSI USE DULU BIAR PINDAH DB
-			remove(tables1)
-			if (remove(tables1) == 0) //jika berhasil
-			{
-				//table dapos deleted
-			}
-			else
-			{
-				//table dapos not exist
-			}
-		}
-		//MASIH SALAH!!
-		else if (strcmp(nthword(test, 2), "COLUMNS") == 0)
-		{
-			if (strcmp(nthword(test, 4), "FROM") == 0)
-			{
-				char *dapos = strtok(nthwork(test, 5), ";");
-				
-				if (remove(tables1) == 0) //jika berhasil
-				{
-					//column dapos deleted
-				}
-				else
-				{
-					//column dapos not exist
-				}	
-			}
-			else
-			{
-				//FROM WHUTT TABLES??
-			}
-		}
-	}
