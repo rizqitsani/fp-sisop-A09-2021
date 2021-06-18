2222eb0b6fff56a3ebb8bc9dc5c3c19900a51a58
Muhammad Daffa <36522826+daffainfo@users.noreply.github.com>
2021-06-18 13:34:31 +0700

    Update database.c

--- database/database.c
+++ database/database.c
@@ -10,29 +10,56 @@
 #include <netinet/in.h>
 #define PORT 8080
 
-int curr_fd = -1;
-int curr_id = -1;
-const int SIZE_BUFFER = sizeof(char) * 300;
+int curr_fd = -1, curr_id = -1;
+const int SIZE_BUFFER = sizeof(char) * 1000;
 
-// Socket setup
-int create_tcp_server_socket();
-
-// Routes & controller
 void *routes(void *argv);
 bool login(int fd, char *username, char *password);
 
 int main()
 {
+	struct sockaddr_in address, new_addr;
+	int fd, new_fd, ret_val, opt = 1;
 	socklen_t addrlen;
-	struct sockaddr_in new_addr;
 	pthread_t tid;
-	char buf[300];
-	int server_fd = create_tcp_server_socket();
-	int new_fd;
+	char buf[1000];
+
+	fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (fd == -1)
+	{
+		fprintf(stderr, "socket failed [%s]\n", strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+
+	address.sin_family = AF_INET;
+	address.sin_port = htons(PORT);
+	address.sin_addr.s_addr = INADDR_ANY;
+
+
+	ret_val = bind(fd, (struct sockaddr *)&address, sizeof(struct sockaddr_in));
+	if (ret_val != 0)
+	{
+		fprintf(stderr, "bind failed");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
+
+	ret_val = listen(fd, 5);
+	if (ret_val != 0)
+	{
+		fprintf(stderr, "listen failed");
+		close(fd);
+		exit(EXIT_FAILURE);
+	}
 
 	while (1)
 	{
-		new_fd = accept(server_fd, (struct sockaddr *)&new_addr, &addrlen);
+		new_fd = accept(fd, (struct sockaddr *)&new_addr, &addrlen);
 		if (new_fd >= 0)
 		{
 			pthread_create(&tid, NULL, &routes, (void *)&new_fd);
@@ -44,9 +71,9 @@ int main()
 void *routes(void *argv)
 {
 	int fd = *(int *)argv;
-	char query[300], buf[300];
+	char query[1000], buf[1000];
 
-	while (read(fd, query, 300) != 0)
+	while (read(fd, query, 1000) != 0)
 	{
 		puts(query);
 
@@ -118,8 +145,6 @@ void *routes(void *argv)
 	close(fd);
 }
 
-/****   Controllers   *****/
-
 bool login(int fd, char *username, char *password)
 {
 	int id = -1;
@@ -141,49 +166,4 @@ bool login(int fd, char *username, char *password)
 		curr_id = id;
 	}
 	return true;
-}
-
-/****   SOCKET SETUP    *****/
-int create_tcp_server_socket()
-{
-	struct sockaddr_in address;
-	int fd, ret_val;
-	int opt = 1;
-
-	/* Step1: create a TCP socket */
-	fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
-	if (fd == -1)
-	{
-		fprintf(stderr, "socket failed [%s]\n", strerror(errno));
-		exit(EXIT_FAILURE);
-	}
-	if (setsockopt(fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-
-	/* Initialize the socket address structure */
-	address.sin_family = AF_INET;
-	address.sin_port = htons(PORT);
-	address.sin_addr.s_addr = INADDR_ANY;
-
-	/* Step2: bind the socket to port 7000 on the local host */
-	ret_val = bind(fd, (struct sockaddr *)&address, sizeof(struct sockaddr_in));
-	if (ret_val != 0)
-	{
-		fprintf(stderr, "bind failed");
-		close(fd);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Step3: listen for incoming connections */
-	ret_val = listen(fd, 5);
-	if (ret_val != 0)
-	{
-		fprintf(stderr, "listen failed");
-		close(fd);
-		exit(EXIT_FAILURE);
-	}
-	return fd;
 }
