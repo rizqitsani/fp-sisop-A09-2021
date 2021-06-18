c454825ac9e98ce12c775122807e4c8d04d05aa3
rizqitsani <rizqitsani@gmail.com>
2021-06-18 13:41:40 +0700

    feat: update client

--- client/client.c
+++ client/client.c
@@ -15,19 +15,41 @@ const int SIZE_BUFFER = sizeof(char) * DATA_BUFFER;
 char username[DATA_BUFFER] = {0};
 bool wait = false;
 
-// SETUP
-int create_tcp_client_socket();
 void *handleInput(void *client_fd);
 void *handleOutput(void *client_fd);
 void getServerOutput(int fd, char *input);
 
-// Controller
 bool login(int, int, char *[]);
 
 int main(int argc, char *argv[])
 {
 	pthread_t tid[2];
-	int client_fd = create_tcp_client_socket();
+
+	struct sockaddr_in address;
+	int client_fd, ret_val;
+	int opt = 1;
+	struct hostent *local_host;
+
+	if ((client_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+	if (setsockopt(client_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+	{
+		perror("setsockopt");
+		exit(EXIT_FAILURE);
+	}
+
+	address.sin_family = AF_INET;
+	address.sin_port = htons(PORT);
+	local_host = gethostbyname("127.0.0.1");
+	address.sin_addr = *((struct in_addr *)local_host->h_addr);
+
+	if (connect(client_fd, (struct sockaddr *)&address, sizeof(struct sockaddr_in)) == -1)
+	{
+		exit(EXIT_FAILURE);
+	}
 
 	if (!login(client_fd, argc, argv))
 	{
@@ -48,18 +70,19 @@ int main(int argc, char *argv[])
 bool login(int fd, int argc, char *argv[])
 {
 	char buf[DATA_BUFFER];
+
 	if (geteuid() == 0)
-	{ // root
+	{
 		write(fd, "LOGIN root", SIZE_BUFFER);
 		puts("LOGIN root");
 		strcpy(username, "root");
 	}
+
 	read(fd, buf, SIZE_BUFFER);
 	puts(buf);
 	return strcmp(buf, "Login success\n") == 0;
 }
 
-/**    SETUP    **/
 void *handleInput(void *client_fd)
 {
 	int fd = *(int *)client_fd;
@@ -68,18 +91,24 @@ void *handleInput(void *client_fd)
 	while (1)
 	{
 		if (wait)
+		{
 			continue;
+		}
+
 		printf("#A09 ");
 		fgets(message, DATA_BUFFER, stdin);
 		char *tmp = strtok(message, "\n");
+
 		if (tmp != NULL)
 		{
 			strcpy(message, tmp);
 		}
+
 		if (strcmp(message, "exit") == 0)
 		{
 			exit(EXIT_SUCCESS);
 		}
+
 		send(fd, message, SIZE_BUFFER, 0);
 		wait = true;
 	}
@@ -107,38 +136,4 @@ void getServerOutput(int fd, char *input)
 		printf("Disconnected from server\n");
 		exit(EXIT_SUCCESS);
 	}
-}
-
-int create_tcp_client_socket()
-{
-	struct sockaddr_in address;
-	int server_fd, ret_val;
-	int opt = 1;
-	struct hostent *local_host; /* need netdb.h for this */
-
-	/* Step1: create a TCP socket */
-	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-	{
-		perror("socket failed");
-		exit(EXIT_FAILURE);
-	}
-	if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
-	{
-		perror("setsockopt");
-		exit(EXIT_FAILURE);
-	}
-
-	/* Let us initialize the server address structure */
-	address.sin_family = AF_INET;
-	address.sin_port = htons(PORT);
-	local_host = gethostbyname("127.0.0.1");
-	address.sin_addr = *((struct in_addr *)local_host->h_addr);
-
-	/* Step2: connect to the TCP server socket */
-	ret_val = connect(server_fd, (struct sockaddr *)&address, sizeof(struct sockaddr_in));
-	if (ret_val == -1)
-	{
-		exit(EXIT_FAILURE);
-	}
-	return server_fd;
 }
